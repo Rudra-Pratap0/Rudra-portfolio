ac30cf76b984d58d2b5495d1d6ae3e98a1f11067
Rudra Pratap Singh Rajawat <rishabthakur08888@gmail.com>
2025-08-16 23:32:24 +0530

    Add files via upload

---
+++ sprits.js
@@ -0,0 +1,5 @@
+// Simple alert for form submission
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for reaching out, Rudra will get back to you soon!");
+});
